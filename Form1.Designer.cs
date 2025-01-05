33a7709f95dc9938cd92816e5817906bb0e72334
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2025-01-05 12:08:12 +0300

    decimal formatlama ile kullanıcı döviz çevirebiliyor

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,17 +30,19 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lblDolar = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtUnitPrice = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioBtnUsd = new System.Windows.Forms.RadioButton();
+            this.radioBtnEuro = new System.Windows.Forms.RadioButton();
+            this.radioBtnGbp = new System.Windows.Forms.RadioButton();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtTotalPrice = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -50,70 +52,70 @@
             // 
             this.lblDolar.AutoSize = true;
             this.lblDolar.Font = new System.Drawing.Font("Calibri", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblDolar.Location = new System.Drawing.Point(249, 45);
+            this.lblDolar.Location = new System.Drawing.Point(157, 34);
             this.lblDolar.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDolar.Name = "lblDolar";
-            this.lblDolar.Size = new System.Drawing.Size(59, 23);
+            this.lblDolar.Size = new System.Drawing.Size(54, 23);
             this.lblDolar.TabIndex = 0;
-            this.lblDolar.Text = "label1";
+            this.lblDolar.Text = "Dolar";
             // 
-            // textBox1
+            // txtUnitPrice
             // 
-            this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textBox1.Location = new System.Drawing.Point(226, 180);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(170, 32);
-            this.textBox1.TabIndex = 4;
+            this.txtUnitPrice.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtUnitPrice.Location = new System.Drawing.Point(325, 180);
+            this.txtUnitPrice.Name = "txtUnitPrice";
+            this.txtUnitPrice.Size = new System.Drawing.Size(170, 32);
+            this.txtUnitPrice.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(122, 186);
+            this.label1.Location = new System.Drawing.Point(221, 186);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(94, 21);
             this.label1.TabIndex = 5;
             this.label1.Text = "Birim Tutar:";
             // 
-            // radioButton1
+            // radioBtnUsd
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(161, 128);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(67, 25);
-            this.radioButton1.TabIndex = 6;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Dolar";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioBtnUsd.AutoSize = true;
+            this.radioBtnUsd.Location = new System.Drawing.Point(260, 128);
+            this.radioBtnUsd.Name = "radioBtnUsd";
+            this.radioBtnUsd.Size = new System.Drawing.Size(67, 25);
+            this.radioBtnUsd.TabIndex = 6;
+            this.radioBtnUsd.TabStop = true;
+            this.radioBtnUsd.Text = "Dolar";
+            this.radioBtnUsd.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // radioBtnEuro
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(247, 128);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(61, 25);
-            this.radioButton2.TabIndex = 7;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Euro";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioBtnEuro.AutoSize = true;
+            this.radioBtnEuro.Location = new System.Drawing.Point(346, 128);
+            this.radioBtnEuro.Name = "radioBtnEuro";
+            this.radioBtnEuro.Size = new System.Drawing.Size(61, 25);
+            this.radioBtnEuro.TabIndex = 7;
+            this.radioBtnEuro.TabStop = true;
+            this.radioBtnEuro.Text = "Euro";
+            this.radioBtnEuro.UseVisualStyleBackColor = true;
             // 
-            // radioButton3
+            // radioBtnGbp
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(334, 128);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(75, 25);
-            this.radioButton3.TabIndex = 8;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Sterlin";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioBtnGbp.AutoSize = true;
+            this.radioBtnGbp.Location = new System.Drawing.Point(433, 128);
+            this.radioBtnGbp.Name = "radioBtnGbp";
+            this.radioBtnGbp.Size = new System.Drawing.Size(75, 25);
+            this.radioBtnGbp.TabIndex = 8;
+            this.radioBtnGbp.TabStop = true;
+            this.radioBtnGbp.Text = "Sterlin";
+            this.radioBtnGbp.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.DarkOrange;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(217, 290);
+            this.button1.Location = new System.Drawing.Point(316, 290);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(162, 32);
             this.button1.TabIndex = 9;
@@ -124,20 +126,20 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(86, 228);
+            this.label2.Location = new System.Drawing.Point(185, 228);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(130, 21);
             this.label2.TabIndex = 11;
             this.label2.Text = "Ödenecek Tutar:";
             // 
-            // textBox2
+            // txtTotalPrice
             // 
-            this.textBox2.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textBox2.Location = new System.Drawing.Point(226, 222);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(170, 32);
-            this.textBox2.TabIndex = 10;
+            this.txtTotalPrice.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTotalPrice.Location = new System.Drawing.Point(325, 222);
+            this.txtTotalPrice.Name = "txtTotalPrice";
+            this.txtTotalPrice.Size = new System.Drawing.Size(170, 32);
+            this.txtTotalPrice.TabIndex = 10;
             // 
             // pictureBox1
             // 
@@ -172,23 +174,47 @@
             this.pictureBox3.TabStop = false;
             this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(330, 34);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 23);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Euro";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(503, 34);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 23);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Sterlin";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(562, 347);
+            this.ClientSize = new System.Drawing.Size(638, 347);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtTotalPrice);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.radioButton3);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.radioBtnGbp);
+            this.Controls.Add(this.radioBtnEuro);
+            this.Controls.Add(this.radioBtnUsd);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtUnitPrice);
             this.Controls.Add(this.lblDolar);
             this.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
@@ -198,6 +224,7 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Döviz Kuru Hesaplama";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
@@ -209,17 +236,19 @@
         #endregion
 
         private System.Windows.Forms.Label lblDolar;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtUnitPrice;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioBtnUsd;
+        private System.Windows.Forms.RadioButton radioBtnEuro;
+        private System.Windows.Forms.RadioButton radioBtnGbp;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtTotalPrice;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,6 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Net.Http;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
@@ -9,14 +8,11 @@ namespace RapidApiCurrencyApp
 {
     public partial class Form1 : Form
     {
-
-        string currency;
-
         public Form1()
         {
             InitializeComponent();
         }
-
+        string currency;
         private async void GetResponseData()
         {
             var client = new HttpClient();
@@ -35,28 +31,44 @@ namespace RapidApiCurrencyApp
             {
                 response.EnsureSuccessStatusCode();
                 var body = await response.Content.ReadAsStringAsync();
-                var json=JObject.Parse(body);
+                var json = JObject.Parse(body);
                 var value = json["result"].ToString();
                 if (currency == "USD")
                 {
-                     lblDolar.Text ="Dolar: "+ value.ToString();
+                    lblDolar.Text = value.ToString();
                 }
-                if (currency=="EUR") 
+                if (currency == "EUR")
                 {
-                     lblDolar.Text ="Euro: "+ value.ToString();
+                    label3.Text = value.ToString();
                 }
-                if(currency== "GBP")
+                if (currency == "GBP")
                 {
-                    lblDolar.Text = "Sterlin: " + value.ToString();
+                    label4.Text = value.ToString();
                 }
             }
         }
-
-
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            decimal unitPrice=decimal.Parse(txtUnitPrice.Text);
+            decimal dollar = decimal.Parse(lblDolar.Text);
+            decimal euro = decimal.Parse(label3.Text);
+            decimal sterlin = decimal.Parse(label4.Text);
+            decimal totalPrice = 0;
+            if (radioBtnUsd.Checked)
+            {
+                totalPrice = unitPrice * dollar;
+            }
+            if (radioBtnEuro.Checked)
+            {
+                totalPrice=unitPrice * euro;
+            }
+            if (radioBtnGbp.Checked)
+            {
+                totalPrice= unitPrice * sterlin;
+            }
+            txtTotalPrice.Text = totalPrice.ToString();
         }
+
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
@@ -74,6 +86,11 @@ namespace RapidApiCurrencyApp
         {
             currency = "EUR";
             GetResponseData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtTotalPrice.Enabled = false;
         }
     }
 }
